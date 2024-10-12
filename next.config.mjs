897a25e5485a6286a4dea9aacb53184319f00f12
Nitin Yadav <nitin.yadav@getzino.com>
2024-10-12 17:05:52 +0530

    new env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1", "localhost"],
+    domains: ["127.0.0.1", "localhost", "go-auth-backend-fr6g.onrender.com"],
     dangerouslyAllowSVG: true,
   },
 };
